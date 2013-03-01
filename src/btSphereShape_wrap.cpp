987a531928dfc0170e8bbdf43cf4fb7676ec6a2b
andres.traks <andres.traks@gmail.com>
2013-03-01 20:09:09 +0000

    [BulletSharpPInvoke] Add CapsuleShape and SphereShape.

---
+++ src/btSphereShape_wrap.cpp
@@ -0,0 +1,16 @@
+#include "btSphereShape_wrap.h"
+
+btSphereShape* btSphereShape_new(btScalar radius)
+{
+    return new btSphereShape(radius);
+}
+
+btScalar btSphereShape_getRadius(btSphereShape* obj)
+{
+	return obj->getRadius();
+}
+
+void btSphereShape_setUnscaledRadius(btSphereShape* obj, float radius)
+{
+	obj->setUnscaledRadius(radius);
+}
