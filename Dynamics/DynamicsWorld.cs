17ae980291f996186fd40589b852b5a3801e8196
andres.traks <andres.traks@gmail.com>
2013-03-02 01:35:08 +0000

    [BulletSharpPInvoke] TypedConstraint.

--- Dynamics/DynamicsWorld.cs
+++ Dynamics/DynamicsWorld.cs
@@ -16,6 +16,36 @@ namespace BulletSharp
         {
         }
 
+        public void AddRigidBody(RigidBody body)
+        {
+            btDynamicsWorld_addRigidBody(_native, body._native);
+        }
+
+        public void AddConstraint(TypedConstraint pconstraint)
+        {
+            AddConstraint(pconstraint, false);
+        }
+
+        public void AddConstraint(TypedConstraint pconstraint, bool disableLinkedCollisions)
+        {
+            btDynamicsWorld_addConstraint(_native, pconstraint._native, disableLinkedCollisions);
+        }
+
+        public TypedConstraint GetConstraint(int index)
+        {
+            return new TypedConstraint(btDynamicsWorld_getConstraint(_native, index));
+        }
+
+        public void RemoveRigidBody(RigidBody body)
+        {
+            BtDynamicsWorld_removeRigidBody(_native, body._native);
+        }
+
+        public void RemoveConstraint(TypedConstraint pconstraint)
+        {
+            btDynamicsWorld_removeConstraint(_native, pconstraint._native);
+        }
+
         public void SetGravity(ref Vector3 v)
         {
             btDynamicsWorld_setGravity(_native, ref v);
@@ -34,31 +64,6 @@ namespace BulletSharp
             return btDynamicsWorld_stepSimulation(_native, timeStep, 1, 1.0f / 60.0f);
         }
 
-        public void AddRigidBody(RigidBody body)
-        {
-            btDynamicsWorld_addRigidBody(_native, body._native);
-        }
-
-        public void removeRigidBody(RigidBody body)
-        {
-            BtDynamicsWorld_removeRigidBody(_native, body._native);
-        }
-
-        public void RemoveConstraint(TypedConstraint pconstraint)
-        {
-            btDynamicsWorld_removeConstraint(_native, pconstraint._native);
-        }
-
-        public void AddConstraint(TypedConstraint pconstraint)
-        {
-            AddConstraint(pconstraint, false);
-        }
-
-        public void AddConstraint(TypedConstraint pconstraint, bool disableLinkedCollisions)
-        {
-            btDynamicsWorld_addConstraint(_native, pconstraint._native, disableLinkedCollisions);
-        }
-
         public Vector3 Gravity
         {
             get
@@ -70,20 +75,25 @@ namespace BulletSharp
             set { btDynamicsWorld_setGravity(_native, ref value); }
         }
 
+        public int NumConstraints
+        {
+            get { return btDynamicsWorld_getNumConstraints(_native); }
+        }
 
         [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
         static extern IntPtr btDynamicsWorld_new(IntPtr dispatcher, IntPtr broadphase, IntPtr solver, IntPtr collisionconfig);
         [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
-        static extern IntPtr CreateBtDynamicsWorld_Specific(IntPtr dispatcher, IntPtr broadphase, IntPtr solver, IntPtr collisionconfig);
+        static extern void btDynamicsWorld_addConstraint(IntPtr dynamicsWorld, IntPtr constraint, bool disableLinkedCollisions);
         [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
         static extern void btDynamicsWorld_addRigidBody(IntPtr obj, IntPtr body);
         [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
-        static extern void BtDynamicsWorld_removeRigidBody(IntPtr obj, IntPtr body);
-
-        [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
-        static extern void btDynamicsWorld_addConstraint(IntPtr dynamicsWorld, IntPtr constraint, bool disableLinkedCollisions);
+        static extern IntPtr btDynamicsWorld_getConstraint(IntPtr obj, int index);
         [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
         static extern void btDynamicsWorld_getGravity(IntPtr dynamicsWorld, [Out] out Vector3 v);
+        [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
+        static extern int btDynamicsWorld_getNumConstraints(IntPtr obj);
+        [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
+        static extern void BtDynamicsWorld_removeRigidBody(IntPtr obj, IntPtr body);
         [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
         static extern void btDynamicsWorld_setGravity(IntPtr dynamicsWorld, [In] ref Vector3 v);
         [DllImport(Native.Dll, CallingConvention = Native.Conv), SuppressUnmanagedCodeSecurity]
