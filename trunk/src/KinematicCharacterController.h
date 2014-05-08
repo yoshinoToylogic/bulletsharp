4aa1eac18e9d65d532eed6b2072ad5fdf24aad41
andres.traks@gmail.com <andres.traks@gmail.com@030a5c38-1ffe-11df-902a-156ec2e33ad3>
2014-05-08 05:16:09 +0000

    Fix double precision build.

--- trunk/src/KinematicCharacterController.h
+++ trunk/src/KinematicCharacterController.h
@@ -13,8 +13,8 @@ namespace BulletSharp
 		KinematicCharacterController(btKinematicCharacterController* native);
 
 	public:
-		KinematicCharacterController(PairCachingGhostObject^ ghostObject, ConvexShape^ convexShape, float stepHeight, int upAxis);
-		KinematicCharacterController(PairCachingGhostObject^ ghostObject, ConvexShape^ convexShape, float stepHeight);
+		KinematicCharacterController(PairCachingGhostObject^ ghostObject, ConvexShape^ convexShape, btScalar stepHeight, int upAxis);
+		KinematicCharacterController(PairCachingGhostObject^ ghostObject, ConvexShape^ convexShape, btScalar stepHeight);
 
 		void SetFallSpeed(btScalar fallSpeed);
 		void SetJumpSpeed(btScalar jumpSpeed);
