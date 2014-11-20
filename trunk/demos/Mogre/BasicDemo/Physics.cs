5ebbcc5e36c7f7d847dd7b90433f12965d09e00d
andres.traks@gmail.com <andres.traks@gmail.com@030a5c38-1ffe-11df-902a-156ec2e33ad3>
2014-11-20 09:14:22 +0000

    Replace AlignedCollisionShapeArray with List<CollisionShape> in all demos.

--- trunk/demos/Mogre/BasicDemo/Physics.cs
+++ trunk/demos/Mogre/BasicDemo/Physics.cs
@@ -1,5 +1,6 @@
 ﻿using BulletSharp;
 using Mogre;
+using System.Collection.Generic;
 
 namespace BasicDemo
 {
@@ -51,7 +52,7 @@ namespace BasicDemo
 
         CollisionDispatcher Dispatcher;
         BroadphaseInterface Broadphase;
-        AlignedCollisionShapeArray CollisionShapes = new AlignedCollisionShapeArray();
+        List<CollisionShape> CollisionShapes = new List<CollisionShape>();
         CollisionConfiguration collisionConf;
 
         ///create 125 (5x5x5) dynamic objects
