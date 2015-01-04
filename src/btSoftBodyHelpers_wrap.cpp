d936424da44e0a1ace93b14852ebf4368aca57d5
andres.traks <andres.traks@gmail.com>
2015-01-04 04:56:55 +0000

    [PInvoke] Implement some more SoftBodyHelpers in managed code.

--- src/btSoftBodyHelpers_wrap.cpp
+++ src/btSoftBodyHelpers_wrap.cpp
@@ -3,6 +3,7 @@
 #include "conversion.h"
 #include "btSoftBodyHelpers_wrap.h"
 
+#ifndef BULLETC_DISABLE_SOFTBODY_HELPERS
 float btSoftBodyHelpers_CalculateUV(int resx, int resy, int ix, int iy, int id)
 {
 	return btSoftBodyHelpers::CalculateUV(resx, resy, ix, iy, id);
@@ -14,7 +15,7 @@ btSoftBody* btSoftBodyHelpers_CreateEllipsoid(btSoftBodyWorldInfo* worldInfo, co
 	VECTOR3_CONV(radius);
 	return btSoftBodyHelpers::CreateEllipsoid(*worldInfo, VECTOR3_USE(center), VECTOR3_USE(radius), res);
 }
-
+#endif
 btSoftBody* btSoftBodyHelpers_CreateFromConvexHull(btSoftBodyWorldInfo* worldInfo, const btScalar* vertices, int nvertices)
 {
 	btVector3* verticesTemp = Vector3ArrayIn(vertices, nvertices);
@@ -35,7 +36,7 @@ btSoftBody* btSoftBodyHelpers_CreateFromTetGenData(btSoftBodyWorldInfo* worldInf
 {
 	return btSoftBodyHelpers::CreateFromTetGenData(*worldInfo, ele, face, node, bfacelinks, btetralinks, bfacesfromtetras);
 }
-#endif
+
 btSoftBody* btSoftBodyHelpers_CreateFromTriMesh(btSoftBodyWorldInfo* worldInfo, const btScalar* vertices, const int* triangles, int ntriangles)
 {
 	return btSoftBodyHelpers::CreateFromTriMesh(*worldInfo, vertices, triangles, ntriangles);
@@ -54,7 +55,7 @@ btSoftBody* btSoftBodyHelpers_CreatePatch(btSoftBodyWorldInfo* worldInfo, const 
 	VECTOR3_CONV(corner11);
 	return btSoftBodyHelpers::CreatePatch(*worldInfo, VECTOR3_USE(corner00), VECTOR3_USE(corner10), VECTOR3_USE(corner01), VECTOR3_USE(corner11), resx, resy, fixeds, gendiags);
 }
-
+#endif
 btSoftBody* btSoftBodyHelpers_CreatePatchUV(btSoftBodyWorldInfo* worldInfo, const btScalar* corner00, const btScalar* corner10, const btScalar* corner01, const btScalar* corner11, int resx, int resy, int fixeds, bool gendiags)
 {
 	VECTOR3_CONV(corner00);
@@ -72,14 +73,14 @@ btSoftBody* btSoftBodyHelpers_CreatePatchUV2(btSoftBodyWorldInfo* worldInfo, con
 	VECTOR3_CONV(corner11);
 	return btSoftBodyHelpers::CreatePatchUV(*worldInfo, VECTOR3_USE(corner00), VECTOR3_USE(corner10), VECTOR3_USE(corner01), VECTOR3_USE(corner11), resx, resy, fixeds, gendiags, tex_coords);
 }
-
+#ifndef BULLETC_DISABLE_SOFTBODY_HELPERS
 btSoftBody* btSoftBodyHelpers_CreateRope(btSoftBodyWorldInfo* worldInfo, const btScalar* from, const btScalar* to, int res, int fixeds)
 {
 	VECTOR3_CONV(from);
 	VECTOR3_CONV(to);
 	return btSoftBodyHelpers::CreateRope(*worldInfo, VECTOR3_USE(from), VECTOR3_USE(to), res, fixeds);
 }
-
+#endif
 void btSoftBodyHelpers_Draw(btSoftBody* psb, btIDebugDraw* idraw)
 {
 	btSoftBodyHelpers::Draw(psb, idraw);
